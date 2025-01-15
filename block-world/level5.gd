8f5e516ed79638c715680e4aaeb2e87414dad7ff
Yird <100542617+Y1rd@users.noreply.github.com>
2025-01-15 11:41:30 -0800

    almost complete game

--- block-world/level5.gd
+++ block-world/level5.gd
@@ -1,5 +1,5 @@
 extends Node2D
-var winvalue = 5
+var winvalue = 9
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +8,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Globalvariable.coincollected == winvalue:
-		get_tree().change_scene_to_file("res://level4.tscn")
+		get_tree().change_scene_to_file("res://level6.tscn")
